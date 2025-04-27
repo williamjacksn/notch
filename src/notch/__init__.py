2e537ba5dc2bf9f298eda9038a090719fb716e25
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-04-27 09:23:47 -0500

    ruff format

--- src/notch/__init__.py
+++ src/notch/__init__.py
@@ -2,23 +2,24 @@ import logging
 import os
 import sys
 
-__version__ = '2023.1'
+__version__ = "2023.1"
 
 log = logging.getLogger(__name__)
 
-def make_log(log_name: str) -> logging.Logger:
-    log_format = os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
-    log_level = os.getenv('LOG_LEVEL', 'INFO')
-    logging.basicConfig(format=log_format, level=logging.DEBUG, stream=sys.stdout)
-    log.debug(f'Initializing logger for {log_name}')
 
-    if not log_level == 'DEBUG':
-        log.debug(f'Changing root log level to {log_level}')
+def make_log(log_name: str) -> logging.Logger:
+    log_format = os.getenv("LOG_FORMAT", "%(levelname)s [%(name)s] %(message)s")
+    log_level = os.getenv("LOG_LEVEL", "INFO")
+    logging.basicConfig(format=log_format, level=logging.DEBUG, stream=sys.stdout)
+    log.debug(f"Initializing logger for {log_name}")
+
+    if not log_level == "DEBUG":
+        log.debug(f"Changing root log level to {log_level}")
     logging.getLogger().setLevel(log_level)
 
-    for log_spec in os.getenv('OTHER_LOG_LEVELS', '').split():
-        logger, level = log_spec.split(':', maxsplit=1)
-        log.debug(f'Changing log level for {logger} to {level}')
+    for log_spec in os.getenv("OTHER_LOG_LEVELS", "").split():
+        logger, level = log_spec.split(":", maxsplit=1)
+        log.debug(f"Changing log level for {logger} to {level}")
         logging.getLogger(logger).setLevel(level)
 
     return logging.getLogger(log_name)
